a8fcc1c210ad90eb04694884b182f2086e0ef488
matheusleal <Matleal64@gmail.com>
2022-02-28 14:30:02 -0500

    Chukkertv VOD Template 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Row from './components/Row';
+import requests from './api/requests';
+import Banner from "./components/Banner";
+import Nav from"./components/Nav";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Banner />
+      <Row isLargeRow={true} title="Upcoming Broadcasts" fetchUrl={requests.fetchUpcoming} />
+      <Row title="Teams" fetchUrl={requests.fetchTeams} />
+      <Row title="World Polo League" fetchUrl={requests.fetchWpl} />
+      <Row title="Daily News" fetchUrl={requests.fetchNews} />
+      <Row title="Overtime" fetchUrl={requests.fetchOvertime} />
+      <Row title="Shows" fetchUrl={requests.fetchShows} />
+      <Row title="Interviews" fetchUrl={requests.fetchInterviews} />
+      <Row title="Aspen Valley Polo Club" fetchUrl={requests.fetchAspenValley} />
+      <Row title="Grand Champions Polo Club" fetchUrl={requests.fetchGcpc} />
     </div>
   );
 }
